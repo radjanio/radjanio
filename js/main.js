0e49f6e794941451338de4f65b7a2cb05687a9ea
radjanio <117517777+radjanio@users.noreply.github.com>
2024-03-23 23:15:29 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,85 +1,79 @@
 function menuShow() {
-            let menuMobile = document.querySelector('.menu-mobile');
-            if (menuMobile.classList.contains('open')) {
-                menuMobile.classList.remove('open');
-                document.querySelector('.icon').src =  "imagens/menu-hamburguer.png";
-            } else {
-                menuMobile.classList.add('open');
-                document.querySelector('.icon').src =  "imagens/cruz.png";  
-            }
-        }
+    let menuMobile = document.querySelector('.menu-mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "imagens/menu-hamburguer.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "imagens/cruz.png";
+    }
+}
 
-        (function() {
-            
-            function Slideshow( element ) {
-                this.el = document.querySelector( element );
-                this.init();
-            }
-            
-            Slideshow.prototype = {
-                init: function() {
-                    this.wrapper = this.el.querySelector( ".slider-wrapper" );
-                    this.slides = this.el.querySelectorAll( ".slide" );
-                    this.previous = this.el.querySelector( ".slider-previous" );
-                    this.next = this.el.querySelector( ".slider-next" );
-                    this.index = 0;
-                    this.total = this.slides.length;
-                    this.timer = null;
-                    
-                    this.action();
-                    this.stopStart();    
-                },
-                _slideTo: function( slide ) {
-                    var currentSlide = this.slides[slide];
-                    currentSlide.style.opacity = 1;
-                    
-                    for( var i = 0; i < this.slides.length; i++ ) {
-                        var slide = this.slides[i];
-                        if( slide !== currentSlide ) {
-                            slide.style.opacity = 0;
-                        }
-                    }
-                },
-                action: function() {
-                    var self = this;
-                    self.timer = setInterval(function() {
-                        self.index++;
-                        if( self.index == self.slides.length ) {
-                            self.index = 0;
-                        }
-                        self._slideTo( self.index );
-                        
-                    }, 3000);
-                },
-                stopStart: function() {
-                    var self = this;
-                    self.el.addEventListener( "mouseover", function() {
-                        clearInterval( self.timer );
-                        self.timer = null;
-                        
-                    }, false);
-                    self.el.addEventListener( "mouseout", function() {
-                        self.action();
-                        
-                    }, false);
+(function() {
+    function Slideshow(element) {
+        this.el = document.querySelector(element);
+        this.init();
+    }
+
+    Slideshow.prototype = {
+        init: function() {
+            this.wrapper = this.el.querySelector(".slider-wrapper");
+            this.slides = this.el.querySelectorAll(".slide");
+            this.index = 0;
+            this.total = this.slides.length;
+            this.timer = null;
+
+            this.action();
+            this.stopStart();
+        },
+        _slideTo: function(slide) {
+            var currentSlide = this.slides[slide];
+            currentSlide.style.opacity = 1;
+
+            for (var i = 0; i < this.slides.length; i++) {
+                var slide = this.slides[i];
+                if (slide !== currentSlide) {
+                    slide.style.opacity = 0;
                 }
-                
-                
-            };
-            
-            document.addEventListener( "DOMContentLoaded", function() {
-                
-                var slider = new Slideshow( "#main-slider" );
-                
-            });
-            
-            
-        })();
+            }
+        },
+        action: function() {
+            var self = this;
+            self.timer = setInterval(function() {
+                self.index++;
+                if (self.index == self.slides.length) {
+                    self.index = 0;
+                }
+                self._slideTo(self.index);
 
-        const botaoModoEscuro = document.getElementById('modo-escuro');
+            }, 3000);
+        },
+        stopStart: function() {
+            var self = this;
+            self.el.addEventListener("mouseover", function() {
+                clearInterval(self.timer);
+                self.timer = null;
 
-        botaoModoEscuro.addEventListener('click', AlternarModoEscuro);
+            }, false);
+            self.el.addEventListener("mouseout", function() {
+                self.action();
 
-        function AlternarModoEscuro() {
-            document.body.classList.toggle('modo-escuro');
+            }, false);
         }
+    };
+
+    document.addEventListener("DOMContentLoaded", function() {
+
+        var slider = new Slideshow("#main-slider");
+
+    });
+
+})();
+
+const botaoModoEscuro = document.getElementById('modo-escuro');
+
+botaoModoEscuro.addEventListener('click', AlternarModoEscuro);
+
+function AlternarModoEscuro() {
+    document.body.classList.toggle('modo-escuro');
+}
